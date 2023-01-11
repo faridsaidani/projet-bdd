319512f52ef3ea45508a7cd7b4ded7990c945716
Farid <kf_saidani@esi.dz>
2023-01-11 20:33:13 +0100

    added navbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import SearchBar from './SearchBar/SearchBar'
+import NavBar from './NavBar/NavBar'
+
+const App = () =>{
+
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App'
 
+const container = document.getElementById("root")
+const root = createRoot(container)
+
+root.render(<App />)
